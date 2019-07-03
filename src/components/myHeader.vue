d3d8a9a68d992a587602ec3d0e8d15853c880c90
YeonjiLim <reusepaper210@gmail.com>
2019-07-03 11:11:44 +0900

    즐겨찾기 기능 추가
    1. 즐겨찾기 기능을 상단 네비게이션 바에 버튼으로 추가
    2. 누르면 내 포트폴리오 사이트를 북마크로 저장함(크롬은 보안성 문제로 ctrl+D를 이용하면 사용 가능하다는 메시지가 나오도록 출력)

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -10,7 +10,9 @@
         <v-btn to="/post" class="white--text" flat>Post</v-btn>
         <v-btn to="/portfolio" class="white--text" flat>Portfolio</v-btn>
         <v-btn to="/login" class="white--text" flat>Login</v-btn>
-        <v-btn class="white--text" flat><vue-bookmark><v-icon>star</v-icon></vue-bookmark></v-btn>
+        <v-btn class="white--text" flat><a href="#" id="favorite" title="즐겨찾기 등록" style="color:white;text-decoration:none">
+          <v-icon>star</v-icon></a>
+        </v-btn>
       </v-toolbar-items>
       <v-toolbar-side-icon class="hidden-sm-and-up white--text" @click="drawer = !drawer"></v-toolbar-side-icon>
     </v-toolbar>
@@ -19,17 +21,17 @@
       <v-list>
         <v-list-tile @click="$router.push('/post')">
           <v-list-tile-content>
-            <v-list-tile-title class="white--text">Post</v-list-tile-title>
+            <v-list-tile-title class="white--text title">Post</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-list-tile @click="$router.push('/portfolio')">
           <v-list-tile-content>
-            <v-list-tile-title class="white--text">Portfolio</v-list-tile-title>
+            <v-list-tile-title class="white--text title">Portfolio</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-list-tile @click="$router.push('/login')">
           <v-list-tile-content>
-            <v-list-tile-title class="white--text">Login</v-list-tile-title>
+            <v-list-tile-title class="white--text title">Login</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
